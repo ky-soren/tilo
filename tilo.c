58f0458b8b876567a016e625b3e7515476433123
Ky Nguyen <kynguyen@kynguyen-macbookpro.roam.corp.google.com>
2018-12-14 00:07:26 -0800

    Adds escape sequences: \x1b[2J and \x1b[H

--- tilo.c
+++ tilo.c
@@ -15,6 +15,9 @@ struct termios orig_termios;
 
 /*** terminal ***/
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -48,6 +51,12 @@ char editorReadKey() {
     return c;
 }
 
+/*** output  ***/
+void editorRefreshScreen() {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 /*** input ***/
 
 void editorProcessKeypress() {
@@ -55,6 +64,8 @@ void editorProcessKeypress() {
 
     switch(c) {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }
@@ -68,6 +79,7 @@ int main() {
     enableRawMode();
 
     while (1) {
+        editorRefreshScreen();
         editorProcessKeypress();
     }
 
